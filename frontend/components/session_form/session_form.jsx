b9b797abfec8aa19e6f7b10cb8bb8cc780d38ade
Wadah Adlan <wadahadlan@gmail.com>
2017-06-20 19:10:10 -0400

    finish for the day

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -11,6 +11,7 @@ class SessionForm extends React.Component {
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.logo = this.logo.bind(this);
+    this.extraPassword = this.extraPassword.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -39,6 +40,21 @@ class SessionForm extends React.Component {
     }
   }
 
+  extraPassword() {
+    if (this.props.formType === 'signup') {
+      return(
+        <p>
+          <input type="password"
+            value={this.state.password}
+            onChange={this.update('password')}
+            className="br5 lightest-dark login-input"
+            placeholder="Confirm Password"
+          />
+        </p>
+      );
+    }
+  }
+
   renderErrors() {
     return(
       <ul>
@@ -56,79 +72,53 @@ class SessionForm extends React.Component {
       <Img src="https://vignette3.wikia.nocookie.net/zimwiki/images/d/d2/Gir_dogsuit.png/revision/latest?cb=20160731215834"/>
     );
   }
-  // render() {
-  //   return (
-  //     <section className="signin-container">
-  //     Welcome to invaderGir!
-  //       <div className="signin-box">
-          // <form onSubmit={this.handleSubmit} className="login-form-box">
-          //   <br/>
-          //   Please {this.props.formType} or {this.navLink()}
-          //   {this.renderErrors()}
-          //   <div className="login-form">
-          //     <br/>
-          //     <label>Username:
-          //       <input type="text"
-          //         value={this.state.username}
-          //         onChange={this.update('username')}
-          //         className="login-input"
-          //       />
-          //     </label>
-          //     <br/>
-          //     <label>Password:
-          //       <input type="password"
-          //         value={this.state.password}
-          //         onChange={this.update('password')}
-          //         className="login-input"
-          //       />
-          //     </label>
-          //     <br/>
-          //     <input type="submit" value="Submit" />
-          //   </div>
-          // </form>
-  //       </div>
-  //     </section>
-  //   );
-  // }
 
-  // {this.logo()}
   render(){
     return(
-      <section className="signin-container">
-        <div className='signin-logo'>
+      <section>
+        <div id='back-zimngir'>
+          <img src='assets/gir-back-to-show'/>
+          <Link to="/">
+            back to Zim 'n' Gir
+          </Link>
         </div>
-        <div className="signin-box">
-        <form onSubmit={this.handleSubmit} className="signin-form">
-          <br/>
-          <span className="fade-break"></span>
-          <p className="formtype-text text-center inline">{this.props.formType}</p>
-          <span className="fade-break fade-break-flip"></span>
-          {this.renderErrors()}
-          <div className="signin-imGir core-dark core-shadow br5">
-            <br/>
-              <input type="text"
-                value={this.state.username}
-                onChange={this.update('username')}
-                className="br5 lightest-dark login-input"
-                placeholder='Username'
-              />
-            <br/>
-            <p>
-              <input type="password"
-                value={this.state.password}
-                onChange={this.update('password')}
-                className="br5 lightest-dark login-input"
-                placeholder="Password"
-              />
-            </p>
-            <br/>
+        <section className="signin-container">
+          <div className='signin-logo'>
           </div>
-          <div className='signin-button'>
-            <input type="submit" value="Submit" className='submit-button right' />
-            <div className="right signin-register-link text-shadow"> {this.navLink()} </div>
+          <div className="signin-box">
+          <form onSubmit={this.handleSubmit} className="signin-form">
+            <br/>
+            <span className="fade-break"></span>
+            <p className="formtype-text text-center inline">{this.props.formType}</p>
+            <span className="fade-break fade-break-flip"></span>
+            {this.renderErrors()}
+            <div className="signin-imGir core-dark core-shadow br5">
+              <br/>
+                <input type="text"
+                  value={this.state.username}
+                  onChange={this.update('username')}
+                  className="br5 lightest-dark login-input"
+                  placeholder='Username'
+                />
+              <br/>
+              <p>
+                <input type="password"
+                  value={this.state.password}
+                  onChange={this.update('password')}
+                  className="br5 lightest-dark login-input"
+                  placeholder="Password"
+                />
+                {this.extraPassword()}
+              </p>
+              <br/>
+            </div>
+            <div className='signin-button'>
+              <input type="submit" value="Submit" className='submit-button right' />
+              <div className="right signin-register-link text-shadow"> {this.navLink()} </div>
+            </div>
+          </form>
           </div>
-        </form>
-        </div>
+        </section>
       </section>
     );
   }
