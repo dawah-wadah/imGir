3d56748ae27d0f54d3f0ef66cab5956dea56fe52
Wadah Adlan <wadahadlan@gmail.com>
2017-06-20 09:43:26 -0400

    migrate to postgres

--- config/application.rb
+++ config/application.rb
@@ -1,21 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module ImGIR
+module ZIMnGir
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
