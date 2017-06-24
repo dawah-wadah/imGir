78a6fb07072fba550fedcc122a897cc2a9664a32
Wadah Adlan <wadahadlan@gmail.com>
2017-06-24 14:50:13 -0400

    upload modal looks nice-ish

--- frontend/components/upload_button/upload_button.jsx
+++ frontend/components/upload_button/upload_button.jsx
@@ -3,14 +3,23 @@ import React from 'react';
 
 const UploadModalContent = () => (
   <div className='uploadModal' onClick={(e)=> e.stopPropagation()}>
-    <div className='upload-icon'>
-      <h1>Upload Icon</h1>
+    <div className='primary-actions'>
+
+    <div className='drag-and-drop'>
+      <img className='upload-giraffe' src='assets/upload-giraffe.png'></img>
+      <img className='upload-pointer' src='assets/upload-pointer.png'></img>
     </div>
     <div className='upload-form'>
-      <form>
-        Form for Upload
-          upload a photo input
-      </form>
+
+      <div className='upload-input-container'>
+      <input type="file" id="global-files-button"
+        className="nodisplay" name="files[]"
+        multiple=""
+        accept=".jpg,.jpeg,.png,.gif,.apng,.tiff,.tif,.bmp,.pdf,.xcf,.webp" />
+      <label className="browse-btn" htmlFor="global-files-button">Browse</label>
+      <p>or drag images here</p>
+    </div>
+  </div>
     </div>
   </div>
 );
