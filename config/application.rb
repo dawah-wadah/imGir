2d99a00fbf57e26dee8c6ea91697627fdf6755bb
Wadah Adlan <wadahadlan@gmail.com>
2017-06-26 16:40:21 -0400

    added s3-region v3

--- config/application.rb
+++ config/application.rb
@@ -18,11 +18,11 @@ module ZIMnGir
 
     config.paperclip_defaults = {
       :storage => :s3,
+      :s3_region => ENV['s3_region'],
       :s3_credentials => {
         :bucket => ENV['s3_bucket'],
         :access_key_id => ENV['s3_access_key_id'],
         :secret_access_key => ENV['s3_secret_access_key'],
-        :s3_region => ENV['s3_region']
       }
     }
 
