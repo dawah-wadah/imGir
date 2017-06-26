5b841912b143c1727eb297da0adcbb7a4ea85873
Wadah Adlan <wadahadlan@gmail.com>
2017-06-26 17:11:20 -0400

    heroku ready(hopefully)

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module ZIMnGir
         :s3_region => ENV['s3_region'],
         :access_key_id => ENV['s3_access_key_id'],
         :secret_access_key => ENV['s3_secret_access_key'],
+        :s3_host_name => "s3-#{ENV["s3_region"]}.amazonaws.com",
+        :url => ":s3_host_name"
       }
     }
 
