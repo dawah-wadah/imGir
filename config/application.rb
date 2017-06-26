6249cff35bdd409b62d26563f328ccc8e29630d3
Wadah Adlan <wadahadlan@gmail.com>
2017-06-26 16:35:14 -0400

    added s3-region v2

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module ZIMnGir
         :bucket => ENV['s3_bucket'],
         :access_key_id => ENV['s3_access_key_id'],
         :secret_access_key => ENV['s3_secret_access_key'],
-        :region => ENV['s3_region']
+        :s3_region => ENV['s3_region']
       }
     }
 
