757f4bf93c146cdb1655acf3061d8894f20c9f03
Wadah Adlan <wadahadlan@gmail.com>
2017-07-15 00:02:44 -0400

    comment styled

--- frontend/components/comments/comments_index_item.jsx
+++ frontend/components/comments/comments_index_item.jsx
@@ -24,21 +24,21 @@ class CommentsIndexItem extends React.Component {
 		this.toggle = this.toggle.bind( this );
 		this.toggleChild = this.toggleChild.bind( this );
 		this.revealVotes = this.revealVotes.bind( this );
-		this.showIcon = this.showIcon.bind(this);
-		this.hideIcon = this.hideIcon.bind(this);
+		this.showIcon = this.showIcon.bind( this );
+		this.hideIcon = this.hideIcon.bind( this );
 	}
 
-	showIcon(e){
+	showIcon( e ) {
 		e.stopPropagation();
-		this.setState({
+		this.setState( {
 			hideReplyIcon: false
-		});
+		} );
 	}
-	hideIcon(e){
+	hideIcon( e ) {
 		e.stopPropagation();
-		this.setState({
+		this.setState( {
 			hideReplyIcon: true
-		});
+		} );
 	}
 
 	toggle() {
@@ -47,9 +47,9 @@ class CommentsIndexItem extends React.Component {
 		} );
 	}
 	toggleChild() {
-		this.props.commentIds.forEach((id) => {
-			this.props.requestOneComment(id);
-		});
+		this.props.commentIds.forEach( ( id ) => {
+			this.props.requestOneComment( id );
+		} );
 		this.setState( {
 			hideReplies: !this.state.hideReplies
 		} );
@@ -66,19 +66,32 @@ class CommentsIndexItem extends React.Component {
 	replies() {
 
 		return (this.state.hideReplies ? <div></div> :
-			<CommentsIndexContainer commentIds={this.props.commentIds} />);
+			<CommentsIndexContainer commentIds={this.props.commentIds} />
+	);
+}
+repliesCount() {
+	if ( this.props.commentIds.length > 0 ) {
+		return (
+			<div>
+
+			<div className='comment-gradient-wrapper'>
+				<div className='comment-gradient'></div>
+			</div>
+
+			<div className='replies-count'>
+			{this.state.hideReplies ?
+				`+ ${this.props.commentIds.length} replies`  : 'Collapse'}
+			</div>
+
+			</div>
+		);
 	}
-	repliesCount() {
-		if ( this.props.commentIds.length > 0 ) {
-			return (<div className='replies-count'>{this.state.hideReplies ?
-				`+ ${this.props.commentIds.length} replies`  : 'Collapse'}</div>);
-		}
-	}
-	revealVotes() {
-		if ( this.props.voted ) {
-			if ( this.props.vote.vote_type === 'Upvote' ) {
-				return (
-					<div className="comment-votes">
+}
+revealVotes() {
+	if ( this.props.voted ) {
+		if ( this.props.vote.vote_type === 'Upvote' ) {
+			return (
+				<div className="comment-votes">
             <img onClick={ this.handleSubmitUpvote }
 							src={window.images.upvote_after}
 							className="vote-arrow" ></img>
@@ -86,10 +99,10 @@ class CommentsIndexItem extends React.Component {
 							src={window.images.downvote_before}
 							className="vote-arrow"></img>
               </div>
-				);
-			} else {
-				return (
-					<div className="comment-votes">
+			);
+		} else {
+			return (
+				<div className="comment-votes">
             <img onClick={ this.handleSubmitUpvote }
 							src={window.images.upvote_before}
 							className="vote-arrow" ></img>
@@ -97,11 +110,11 @@ class CommentsIndexItem extends React.Component {
 							src={window.images.downvote_after}
 							className="vote-arrow"></img>
               </div>
-				);
-			}
-		} else {
-			return (
-				<div className="comment-votes">
+			);
+		}
+	} else {
+		return (
+			<div className="comment-votes">
           <img onClick={ this.handleSubmitUpvote }
 						src={window.images.upvote_before}
 						className="vote-arrow" ></img>
@@ -109,85 +122,83 @@ class CommentsIndexItem extends React.Component {
 						src={window.images.downvote_before}
 						className="vote-arrow"></img>
             </div>
-			);
-		}
+		);
 	}
+}
 
-	vote( upOrDown ) {
-		if ( this.props.voted ) {
-			return this.props.vote;
-		} else {
-			const vote = {
-				voter_id: this.props.accountId,
-				votable_id: this.props.commentId,
-				votable_type: 'Comment',
-				vote_type: `${upOrDown}`,
-			};
-			return vote;
-		}
+vote( upOrDown ) {
+	if ( this.props.voted ) {
+		return this.props.vote;
+	} else {
+		const vote = {
+			voter_id: this.props.accountId,
+			votable_id: this.props.commentId,
+			votable_type: 'Comment',
+			vote_type: `${upOrDown}`,
+		};
+		return vote;
 	}
-	handleSubmitUpvote( e ) {
-		e.preventDefault();
-		if ( this.props.loggedIn ) {
-			this.props.toggleUpvote( this.vote( 'Upvote' ), this.props.voted );
-		} else {
-			this.props.history.push( '/login' );
-		}
+}
+handleSubmitUpvote( e ) {
+	e.preventDefault();
+	if ( this.props.loggedIn ) {
+		this.props.toggleUpvote( this.vote( 'Upvote' ), this.props.voted );
+	} else {
+		this.props.history.push( '/login' );
 	}
-	handleSubmitDownvote( e ) {
-		e.preventDefault();
-		if ( this.props.loggedIn ) {
-			this.props.toggleDownvote( this.vote( 'Downvote' ), this.props.voted );
-		} else {
-			this.props.history.push( '/login' );
-		}
+}
+handleSubmitDownvote( e ) {
+	e.preventDefault();
+	if ( this.props.loggedIn ) {
+		this.props.toggleDownvote( this.vote( 'Downvote' ), this.props.voted );
+	} else {
+		this.props.history.push( '/login' );
 	}
-	render() {
-		if ( this.props.comment.id ) {
-			return (
-				<div className='child' key={this.props.comment.id}
+}
+render() {
+	if ( this.props.comment.id ) {
+		return (
+			<div className='child' key={this.props.comment.id}
 					>
         <div className="comment" onClick={this.toggleChild} onMouseEnter={(e) => this.showIcon(e)}
 				onMouseLeave={(e) => this.hideIcon(e)}>
 
           <div className="comment-info">
             <div className="author">
-							<div className="comment-user-name cf">
                 <div className="comment-username">{this.props.comment.username}</div>
                 <div className="comment-username spacer">{this.props.comment.points} pts</div>
+								<div className='time-since-posted spacer'>
+									<Moment fromNow>
+										{this.props.comment.created_at}
+									</Moment>
+								</div>
               </div>
-              <div className='time-since-posted spacer'>
-                <Moment fromNow>
-                  {this.props.comment.created_at}
-                </Moment>
-              </div>
-            </div>
             <div className="body">
               <span className='comment-body'>
                 {this.props.comment.body}
               </span>
             </div>
-          </div>
-            {!this.state.hideReplyIcon ?
+					</div>
+						{!this.state.hideReplyIcon ?
 
 							<div className="comment-reply-icon" onClick={this.toggle}>
-  						{this.state.hideReplyForm
-  							? <div className='reply-icon' >Reply </div>
-  							: <div className='reply-icon' >Close Form </div>}
-  					</div>
-					: null}
+								{this.state.hideReplyForm
+									? <div className='reply-icon' >Reply </div>
+									: <div className='reply-icon' >Close Form </div>}
+								</div>
+								: null}
         </div>
-        {this.replyForm()}
         <p className='reply-icon' onClick={this.toggleChild}>
           {this.repliesCount()}
         </p>
+				{this.replyForm()}
         {this.replies()}
       </div>
-			);
-		} else {
-			return null;
-		}
+		);
+	} else {
+		return null;
 	}
+}
 
 }
 
