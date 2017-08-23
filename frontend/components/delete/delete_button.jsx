fd45c8e277faa07d2d9e20cb1f08196d77f8ca98
Wadah Adlan <wadahadlan@gmail.com>
2017-08-23 00:25:26 -0400

    delete button intro

---
+++ frontend/components/delete/delete_button.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const DeleteButton = ({deleteFunction}) => {
+  debugger
+  return (
+    <button onClick={() => this.deleteFunction()}
+      className='deleteButton'>
+      Delete
+    </button>
+  );
+};
--- frontend/components/user/user_gallery.jsx
+++ frontend/components/user/user_gallery.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PostIndexItem from '../posts/post_index_item';
+import { DeleteButton } from '../delete/delete_button';
 
 export default class UserGallery extends React.Component {
   constructor(props) {
@@ -8,7 +9,9 @@ export default class UserGallery extends React.Component {
 
   render() {
     const allPosts = this.props.posts.sort((a, b) => (b.created_at - a.created_at)).map((post) => (
-      <PostIndexItem post={post} key={post.id}/>
+      <PostIndexItem post={post} key={post.id}>
+        <DeleteButton/>
+      </PostIndexItem>
       ));
     return (
       <div className='post-index-container user'>
